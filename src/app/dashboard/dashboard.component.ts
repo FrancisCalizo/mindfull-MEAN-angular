a38c9a2126c049a378560771a0a04f27a2575f08
Francis Calizo <francisacalizo@gmail.com>
2018-03-15 14:08:33 -0400

    Create connection of Dashboard to Angular file

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthService } from "../services/auth.service";
+import { DashboardService } from "../services/dashboard.service";
+import { environment } from "../../environments/environment";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +10,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  logoutError: string;
+  entryListError: string;
+  entries: any;
+  currentUser: string;
 
-  constructor() { }
+  constructor(
+    private myAuthService: AuthService,
+    private myRouter: Router,
+    private myDashboardService: DashboardService
+  ) {}
 
   ngOnInit() {
+    this.myAuthService
+      .checklogin()
+      // If success, we are logged in.
+      .then(resultFromApi => {
+        this.currentUser = resultFromApi;
+        console.log("user is: ", resultFromApi);
+        this.getThePhones()
+      })
+
+      // Even if you don't do anything on error, catch to avoid a console error.
+      .catch(err => {
+        console.log(err);
+        this.myRouter.navigate(["/"]);
+      });
+    // this.getThePhones();
   }
 
+  getThePhones() {
+    this.myDashboardService.retrieveEntries()
+    .subscribe(allThePhones => {
+      // console.log("allThePhones: ", allThePhones)
+        this.entries = allThePhones;
+        console.log("phones", this.entries)
+      },
+      () => {
+        this.entryListError = "Sorry, no phones.";
+      }
+    );
+  } // close getThePhones()
+
+  logMeOutPls() {
+    this.myAuthService
+      .logout()
+      .then(() => {
+        this.myRouter.navigate(["/"]);
+      })
+      .catch(() => {
+        this.logoutError = "Log out went bad.";
+      });
+  } // close logMeOutPls()
 }
--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { environment } from '../../environments/environment';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DashboardService {
 
-  constructor() { }
+  constructor( private http: Http ) { }
 
+  retrieveEntries(){
+    return this.http.get(`${environment.apiBase}/phones`,
+    { withCredentials: true })
+    .map(res => res.json())
+  }
 }
